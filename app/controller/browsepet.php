27a28f1a7454c7d0eca812e06273fd5a27b78b1c
Shlok Talati <talati.shlok3@gmail.com>
2024-03-10 22:37:02 +0530

    Created BrowsePet and Faq Controller

--- app/controller/browsepet.php
+++ app/controller/browsepet.php
@@ -1,1 +1,11 @@
+<?php require(ROOT . 'app/model/Pet.php'); ?>
+
+<?php
+
+$Pet = new Pet();
+
+$petData = $Pet->get_pet_data();
+?>
+
+
 <?php require(ROOT .'app/controller/base.php'); ?>
--- app/controller/faq.php
+++ app/controller/faq.php
@@ -1,1 +1,11 @@
+<?php require(ROOT . 'app/model/Info.php'); ?>
+
+<?php
+$Info = new Info();
+
+$faq = $Info->get_faq();
+
+?>
+
+
 <?php require(ROOT .'app/controller/base.php'); ?>
