370cbf21a88b3310669357dad5d34dbc40838e07
Shlok Talati <talati.shlok3@gmail.com>
2024-04-20 09:18:44 +0530

    Enabled Warnings Again

--- init.php
+++ init.php
@@ -1,8 +1,9 @@
-<?php session_start(); ?>
-<?php error_reporting(0); ?>
+<?php session_start();
+// error_reporting(0);
+?>
 
 <!-- Define the root path -->
-<?php define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/petmarket/'); 
+<?php define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/petmarket/');
 
 $parsed_path = parse_url($_SERVER['REQUEST_URI']);
 $request_uri_path = str_replace('/petmarket', '', $parsed_path['path']); // Remove the /petmarket from the URL
